34b44972b119ff92f295fa1c29570e5d57fe1de5
bjjumba <95844148+bjjumba@users.noreply.github.com>
2022-12-16 14:49:06 +0300

    network checker

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import MainFrame from './components/MainFrame';
 import Navbar from './components/Navbar';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 
 function App() {
   
+  //check network changes in the mobile app
+const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+useEffect(() => {
+    // Update network status
+    const handleStatusChange = () => {
+      setIsOnline(navigator.onLine);
+    };
+
+    // Listen to the online status
+    window.addEventListener('online', handleStatusChange);
+
+    // Listen to the offline status
+    window.addEventListener('offline', handleStatusChange);
+     //send a toast message
+     isOnline?toast.success("You are Online",
+     {
+      icon:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 19.5v-.75a7.5 7.5 0 00-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75M6 18.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+    </svg>,
+    position: toast.POSITION.BOTTOM_CENTER
+     })
+     :
+     toast.error("Please Check Your Network connection",
+     {icon:
+     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+     <path strokeLinecap="round" strokeLinejoin="round" d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z" />
+     </svg>,
+     position: toast.POSITION.BOTTOM_CENTER
+   })
+    // Specify how to clean up after this effect for performance improvment
+    return () => {
+      window.removeEventListener('online', handleStatusChange);
+      window.removeEventListener('offline', handleStatusChange);
+    };
+}, [isOnline]);
 
   return (
     <>
       <Navbar/>
       <MainFrame/>
+      <ToastContainer/>
     </>
   );
 }
